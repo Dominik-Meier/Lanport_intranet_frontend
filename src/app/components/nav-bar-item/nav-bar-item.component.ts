583c841ddf740587ef4c1fb1cc1c7214da8275c4
Dominik Meier <diminik.meier2@students.fhnw.ch>
2020-03-24 11:57:49 +0100

    working on nav bar

--- src/app/components/nav-bar-item/nav-bar-item.component.ts
+++ src/app/components/nav-bar-item/nav-bar-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar-item',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar-item.component.scss']
 })
 export class NavBarItemComponent implements OnInit {
+  @Input() name: string;
 
   constructor() { }
 
--- src/app/services/nav-bar-item.service.ts
+++ src/app/services/nav-bar-item.service.ts
@@ -12,6 +12,7 @@ export class NavBarItemService {
     //TODO test cases remove those later
     this.addNavBarItem( new NavBarItem('test1', ['test1-1', 'test1-2']));
     this.addNavBarItem( new NavBarItem('test2', ['test2-1', 'test2-2']));
+    this.addNavBarItem( new NavBarItem('test3', ['test3-1', 'test3-2']));
   }
 
   getNavBarItems() {
