09be67e00031b01c9f968c33b4adb2f6fd7bbf4b
Dominik Meier <dominik.meier@elca.ch>
2021-08-14 14:22:46 +0200

    added missing beamer component

---
+++ src/app/views/beamer-view/beamer-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beamer-view',
+  templateUrl: './beamer-view.component.html',
+  styleUrls: ['./beamer-view.component.scss']
+})
+export class BeamerViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
