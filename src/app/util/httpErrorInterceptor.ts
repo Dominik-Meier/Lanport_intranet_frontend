949d2ff3e7085ac90f30217a7682035ae584bd17
Dominik Meier <dominik.meier@elca.ch>
2021-08-31 16:37:28 +0200

    add simple jwt session

--- src/app/util/httpErrorInterceptor.ts
+++ src/app/util/httpErrorInterceptor.ts
@@ -1,4 +1,4 @@
-import {Injectable, Injector, isDevMode} from '@angular/core';
+import {Injectable, Injector} from '@angular/core';
 import {
   HttpEvent,
   HttpInterceptor,
@@ -10,7 +10,10 @@ import {Observable} from 'rxjs';
 import {ErrorMsgService} from '../services/error-msg.service';
 import {catchError} from 'rxjs/operators';
 import {ErrorMsg} from '../models/errorMsg';
-import {TokenStorageService} from '../services/token-storage.service';
+import {AuthService} from '../services/auth-service.service';
+/*
+https://github.com/IraErshova/angular-authorization/blob/master/client/src/app/services/token.interceptor.ts
+*/
 
 /** Pass untouched request through to the next request handler. */
 @Injectable()
@@ -18,16 +21,18 @@ export class HttpErrorInterceptor implements HttpInterceptor {
   private errorMsgService: ErrorMsgService;
   private TOKEN_HEADER_KEY = 'Authorization';
 
-  constructor(private injector: Injector, private tokenService: TokenStorageService) {
+  constructor(private injector: Injector, private authService: AuthService) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this.errorMsgService = this.injector.get(ErrorMsgService);
-    const token = this.tokenService.getToken();
+    // Set jwt token
+    const token = this.authService.getToken();
     if (token) {
       req = req.clone({headers: req.headers.set(this.TOKEN_HEADER_KEY, 'Bearer ' + token)});
     }
 
+    // Handle backend error responses
     // @ts-ignore
     return next.handle(req).pipe(
       catchError( (err  => {
@@ -36,6 +41,10 @@ export class HttpErrorInterceptor implements HttpInterceptor {
           errMsg =  new ErrorMsg('Request do Backend failed reason unknown.', 'Server Error: ');
         } else if (err.status === 401) {
           errMsg =  new ErrorMsg('Please login at main page first.', 'Unauthorized: ');
+          this.authService.signOut();
+        } else if (err.status === 403) {
+          errMsg =  new ErrorMsg('Please login at main page first.', 'Unauthorized: ');
+          this.authService.signOut();
         } else if (err.status >= 404 && err.status < 600) {
           errMsg = new ErrorMsg(err.error.server_error, err.error.server_message);
         }
